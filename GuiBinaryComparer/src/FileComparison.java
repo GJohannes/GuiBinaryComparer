85e850f5fdd951930d661ba0b4c986c0a271bbaf
GJohannes <johannes.guenthoer@t-online.de>
2018-06-13 15:46:40 +0200

    extracted file comparison from folder comparison

---
+++ GuiBinaryComparer/src/FileComparison.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class FileComparison {
+
+	public boolean areFilesBinaryEqual(File fileA, File fileB) {
+		Path pathFileA = Paths.get(fileA.getPath());
+		Path pathFileB = Paths.get(fileB.getPath());
+
+		byte[] bytesFileA = getBinarieFromLocation(pathFileA);
+		byte[] bytesFileB = getBinarieFromLocation(pathFileB);
+
+		if (bytesFileA.length != bytesFileB.length) {
+			return false;
+		} else {
+			for (int i = 0; i < bytesFileA.length; i++) {
+				if (bytesFileA[i] != bytesFileB[i]) {
+
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private byte[] getBinarieFromLocation(Path path) {
+		try {
+			return Files.readAllBytes(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}	
+}
