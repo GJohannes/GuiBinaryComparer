2b8db7d253c69ac41264a84037045866f7e5306e
GJohannes <johannes.guenthoer@t-online.de>
2018-06-21 10:17:15 +0200

    -added error message to folder comparison if input cant be read correctly -added a local image loader to controller

--- GuiBinaryComparer/src/MainWindowController.java
+++ GuiBinaryComparer/src/MainWindowController.java
@@ -23,6 +23,7 @@ import javafx.util.Duration;
 
 public class MainWindowController {
 	private MainWindow mainWindow;
+	private ImageLoader images = new ImageLoader();
 	
 	public MainWindowController(MainWindow mainWindow) {
 		this.mainWindow = mainWindow;
@@ -30,22 +31,27 @@ public class MainWindowController {
 		mainWindow.compareFile.setOnAction(e -> {
 			if (this.areFoldersInSegmentBinaryEqual(mainWindow.segmentA, mainWindow.segmentB)) {
 				mainWindow.resultLabel.setText("Files are binary equal");
-				mainWindow.resultLabel.setGraphic(mainWindow.images.getGreenCheckIcon());
+				mainWindow.resultLabel.setGraphic(images.getGreenCheckIcon());
 			} else {
 				mainWindow.resultLabel.setText("not equal Files");
-				mainWindow.resultLabel.setGraphic(mainWindow.images.getRedUnCheck());
+				mainWindow.resultLabel.setGraphic(images.getRedUnCheck());
 			}
 		});
 		
 		mainWindow.compareFolder.setOnAction(e -> {
 			GuiAndWorkerSharedValues.setTotalFiles(0);
-			this.areFoldersInSegmentBinaryEqual(mainWindow.folderSegmentA, mainWindow.folderSegmentB);
+			if(!this.areFoldersInSegmentBinaryEqual(mainWindow.folderSegmentA, mainWindow.folderSegmentB)) {
+				mainWindow.informationMappingFilesA.setText("Select Directory");
+				mainWindow.informationMappingFilesA.setGraphic(images.getRedUnCheck());
+				mainWindow.informationMappingFilesB.setText("Select Directory");
+				mainWindow.informationMappingFilesB.setGraphic(images.getRedUnCheck());
+			}
 		});
 		
 		//update permanent 
 		Timeline updatingGuiTask = new Timeline(new KeyFrame(Duration.millis(50), e -> {
 			mainWindow.mappedTotalFiles.setText(Integer.toString(GuiAndWorkerSharedValues.getTotalFiles()) + " - Files Mapped");
-			System.out.println(mainWindow.folderResults.getChildren().size());
+			//System.out.println(mainWindow.folderResults.getChildren().size());
 		}));
 		
 		updatingGuiTask.setCycleCount(Timeline.INDEFINITE);
@@ -77,65 +83,68 @@ public class MainWindowController {
 			}
 		}
 
-		FolderComparison folderComparison = new FolderComparison(directoryOrFileA, directoryOrFileB, mainWindow);
-		Thread thread = new Thread(folderComparison);
 
-		folderComparison.messageProperty().addListener(e -> {
-			if (folderComparison.getMessage().equals(STATES.FINISHED_FIRST_COMPARISON.toString())) {
-				mainWindow.informationMappingFilesA.setGraphic(mainWindow.images.getGreenCheckIcon());
-				mainWindow.informationMappingFilesA.setText("Finished Mapping");
-			} else if (folderComparison.getMessage().equals(STATES.FINISHED_SECOND_COMPARISON.toString())) {
-				mainWindow.informationMappingFilesB.setGraphic(mainWindow.images.getGreenCheckIcon());
-				mainWindow.informationMappingFilesB.setText("Finished Mapping");
-				// Thread sometimes updates so fast that up can not pick it up. first message
-				// always already came if this message is displayed.
-				// duplicate resolves the bug of not displaying first message result correctly
-				mainWindow.informationMappingFilesA.setGraphic(mainWindow.images.getGreenCheckIcon());
-				mainWindow.informationMappingFilesA.setText("Finished Mapping");
-			}
-		});
-
-		folderComparison.valueProperty().addListener(e -> {
-			ArrayList<HBox> tempList = new ArrayList<>();
-			tempList = folderComparison.getValue();
-
-			for (int i = 0; i < tempList.size(); i++) {
-				// if box is not already placed on GUI then add
-				if (!mainWindow.folderResults.getChildren().contains(tempList.get(i))) {
-					mainWindow.folderResults.getChildren().add(tempList.get(i));
-				}
-			}
-		});
-
-		folderComparison.progressProperty().addListener(e -> {
-			mainWindow.progressBar.setProgress(folderComparison.getProgress());
-			Number number = Math.round(folderComparison.getProgress() * 100);
-			mainWindow.percentageDone.setText(number.intValue() + "%");
-		});
-
-		folderComparison.setOnSucceeded(e -> {
-			mainWindow.compareFolder.setDisable(false);
-		});
-		// get values from textfield and read content as files
+		
 
 		// first case is that folders/directories are in both segments
 		if (directoryOrFileA != null && directoryOrFileA.exists() && directoryOrFileA.isDirectory()
 				&& directoryOrFileB != null && directoryOrFileB.exists() && directoryOrFileB.isDirectory()) {
 
 			mainWindow.folderResults.getChildren().clear();
-			mainWindow.informationMappingFilesA.setGraphic(mainWindow.images.getWaitingIcon());
+			mainWindow.informationMappingFilesA.setGraphic(images.getWaitingIcon());
 			mainWindow.informationMappingFilesA.setText("Started Mapping ");
 			mainWindow.informationMappingFilesB.setText("Started Mapping ");
-			mainWindow.informationMappingFilesB.setGraphic(mainWindow.images.getWaitingIcon());
+			mainWindow.informationMappingFilesB.setGraphic(images.getWaitingIcon());
 			mainWindow.informationMappingFilesB.setAlignment(Pos.CENTER_RIGHT);
 			mainWindow.compareFolder.setDisable(true);
 
-			thread.start();
+			FolderComparison folderComparison = new FolderComparison(directoryOrFileA, directoryOrFileB, mainWindow);
+			Thread thread = new Thread(folderComparison);
 
+			folderComparison.messageProperty().addListener(e -> {
+				if (folderComparison.getMessage().equals(STATES.FINISHED_FIRST_COMPARISON.toString())) {
+					mainWindow.informationMappingFilesA.setGraphic(images.getGreenCheckIcon());
+					mainWindow.informationMappingFilesA.setText("Finished Mapping");
+				} else if (folderComparison.getMessage().equals(STATES.FINISHED_SECOND_COMPARISON.toString())) {
+					mainWindow.informationMappingFilesB.setGraphic(images.getGreenCheckIcon());
+					mainWindow.informationMappingFilesB.setText("Finished Mapping");
+					// Thread sometimes updates so fast that up can not pick it up. first message
+					// always already came if this message is displayed.
+					// duplicate resolves the bug of not displaying first message result correctly
+					mainWindow.informationMappingFilesA.setGraphic(images.getGreenCheckIcon());
+					mainWindow.informationMappingFilesA.setText("Finished Mapping");
+				}
+			});
+
+			folderComparison.valueProperty().addListener(e -> {
+				ArrayList<HBox> tempList = new ArrayList<>();
+				tempList = folderComparison.getValue();
+
+				for (int i = 0; i < tempList.size(); i++) {
+					// if box is not already placed on GUI then add
+					if (!mainWindow.folderResults.getChildren().contains(tempList.get(i))) {
+						mainWindow.folderResults.getChildren().add(tempList.get(i));
+					}
+				}
+			});
+
+			folderComparison.progressProperty().addListener(e -> {
+				mainWindow.progressBar.setProgress(folderComparison.getProgress());
+				Number number = Math.round(folderComparison.getProgress() * 100);
+				mainWindow.percentageDone.setText(number.intValue() + "%");
+			});
+
+			folderComparison.setOnSucceeded(e -> {
+				mainWindow.compareFolder.setDisable(false);
+			});
+			
+			thread.start();
+			
 			// return folderComparison.areFoldersEqual(directoryOrFileA, directoryOrFileB,
 			// target);
 
 			// second case is that both values represent files
+			return true;
 		} else if (directoryOrFileA != null && directoryOrFileA.exists() && directoryOrFileA.isFile()
 				&& directoryOrFileB != null && directoryOrFileB.exists() && directoryOrFileB.isFile()) {
 			return fileComparison.areFilesBinaryEqual(directoryOrFileA, directoryOrFileB);
@@ -143,7 +152,6 @@ public class MainWindowController {
 			return false;
 		}
 
-		return false;
 	}
 
 	
